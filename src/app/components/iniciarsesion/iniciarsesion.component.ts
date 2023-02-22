201abcf855008016975434056cf7fc6475432c73
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-02-21 18:14:56 -0600

    CambiosNadia

--- src/app/components/iniciarsesion/iniciarsesion.component.ts
+++ src/app/components/iniciarsesion/iniciarsesion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { LoginService } from 'src/app/services/Login/log.service';
 
 @Component({
   selector: 'app-iniciarsesion',
@@ -7,11 +8,34 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./iniciarsesion.component.css']
 })
 export class IniciarsesionComponent {
+  email:string="";
+  password:string="";
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private login:LoginService) { }
 
   navegar()
   {
     this.router.navigate(['/registrar']);
+    this.router.navigate(['/menu']);
+  }
+  abrirC()
+  {
+    const form = { 
+      email: this.email,   
+      password: this.password,
+    }
+    console.log( JSON.stringify(form));
+
+    this.login.login(form).
+    subscribe(
+      (res) => {
+        alert(res)
+      },
+      (err) => {
+        alert(err)
+      }
+    );
+    
+
   }
 }
